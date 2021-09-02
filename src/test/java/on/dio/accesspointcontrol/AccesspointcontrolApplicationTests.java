08ff082b16280f79fab2f25c02e7eca16cd90d88
Ademir <ademir.m.junior@gmail.com>
2021-09-02 11:52:38 -0300

    Initial commit

---
+++ src/test/java/on/dio/accesspointcontrol/AccesspointcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package on.dio.accesspointcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesspointcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
